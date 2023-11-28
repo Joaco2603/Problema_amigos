7e8b6580eb1a1099529604ffa1088807d4399c62
Joaco2603 <111933983+Joaco2603@users.noreply.github.com>
2023-11-28 14:50:12 -0600

    Primer filter funcionando

--- filters/filter_guess.py
+++ filters/filter_guess.py
@@ -1,6 +1,33 @@
 
+friends_cache = [];
+friends = [];
+guess_names =[];
+guests_of_my_guests = [];
+
 
 def filter_guess(guess):
     for data in guess:
-        print(data.name);
-        print(data.friends.name);
+         friends_cache.append(data.friends.name);
+         guess_names.append(data.name);
+    union_friends();
+    seek_guess_friends();
+        
+        
+def union_friends():
+    for data in friends_cache:
+        for friend in data:
+            friends.append(friend);
+            
+    del friends_cache[:]
+    
+
+def seek_guess_friends():
+    
+    for guess in guess_names:
+        if guess in friends:
+            print(guess);
+    
+    # if friends[0].find("Ca") != -1:
+    #     print("No");
+    # else:
+    #     print("Si");
