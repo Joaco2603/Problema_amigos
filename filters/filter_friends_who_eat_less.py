f7f56b2a68b5ed4c461bcc261ae3e7beec704259
Joaco2603 <111933983+Joaco2603@users.noreply.github.com>
2023-12-03 10:35:34 -0600

    Funcion agregar a listas

--- filters/filter_friends_who_eat_less.py
+++ filters/filter_friends_who_eat_less.py
@@ -1,8 +1,10 @@
 from patterns.Singleton import singleton_resultado;
 
+Repetition = [];
+
 def filter_friends_who_eat_less():
-    print(singleton_resultado.objeto.data);
     for guess in singleton_resultado.objeto.data:
-        for name, dishes in guess.friends.name,guess.friends.dishes:
-            dictionary_friends = dict(name=name,dishes=dishes);
-            print(dictionary_friends);
+        for name, dishes in zip(guess.friends.name,guess.friends.dishes):
+            Repetition.append(name);
+            dictionary_friends = dict(name=name,dishes=dishes)
+            print(Repetition);
